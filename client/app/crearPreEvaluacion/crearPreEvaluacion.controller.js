3d7390790738dc2726abf6a2a197a5ee46c71f82
rapazz <moises.bravo@rapazz.cl>
2014-08-12 16:27:46 -0400

    se Agregan paginas faltantes

--- client/app/crearPreEvaluacion/crearPreEvaluacion.controller.js
+++ client/app/crearPreEvaluacion/crearPreEvaluacion.controller.js
@@ -3,4 +3,15 @@
 angular.module('gestionApp')
   .controller('CrearpreevaluacionCtrl', function ($scope) {
     $scope.message = 'Hello';
+
+        $scope.proyecto={}
+        $scope.proyecto.nombreDelProyecto='Proyecto 1';
+        $scope.proyecto.nombreUsuario="Moisés Bravo";
+        $scope.proyecto.fechaSolicitud="10/08/2014";
+        $scope.proyecto.empresa="KCC";
+        $scope.proyecto.departamento="Informatica"
+        $scope.proyecto.resumenEjecutivo='Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+        $scope.listas={}
+        $scope.listas.tipoAprobador =['Gerente de Pryecto','Key User','Patrocinador']
+        $scope.listas.tipoAdjunto=['Doc Pre Evaluacion','Documento de Inicio','Doc. Solicitud']
   });
--- client/app/misProyectos/misProyectos.controller.js
+++ client/app/misProyectos/misProyectos.controller.js
@@ -10,10 +10,30 @@ angular.module('gestionApp')
                 nombreProyecto:'Proyecto 1',
                estado:'nueva',
                 fechaTermino:'10/08/2015',
-                etapa:'En Pre-evaluación',
+                etapa:'Pre-Evaluación',
                 empresa:'KCC',
                 oneOff:'',
                 oneGoing:''
+            },
+            {
+                id:'2',
+                nombreProyecto:'Proyecto 2',
+                estado:'nueva',
+                fechaTermino:'12/08/2015',
+                etapa:'Evaluación',
+                empresa:'KHSA',
+                oneOff:'15000',
+                oneGoing:'2500'
+            },
+            {
+                id:'3',
+                nombreProyecto:'Proyecto 3',
+                estado:'nueva',
+                fechaTermino:'12/08/2015',
+                etapa:'Ejecucion',
+                empresa:'KCCA',
+                oneOff:'25000',
+                oneGoing:'2500'
             }
             ]
 
